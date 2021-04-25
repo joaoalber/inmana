41239c0e9965fb9a0117d0c9df557d7baa90afa8
Joao Alberto <jooalberto1@gmail.com>
2021-04-24 21:47:19 -0300

    adds notification expiration scheduler using GenServer

---
+++ lib/inmana/supplies/scheduler.ex
@@ -0,0 +1,28 @@
+defmodule Inmana.Supplies.Scheduler do
+  use GenServer
+
+  alias Inmana.Supplies.ExpirationNotifier
+
+  def start_link(_state) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  @impl true
+  def init(state \\ %{}) do
+    schedule_notification()
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(msg, state) do
+    ExpirationNotifier.send()
+
+    schedule_notification()
+
+    {:noreply, state}
+  end
+
+  defp schedule_notification do
+    Process.send_after(self(), :generate, 1000 * 10)
+  end
+end
