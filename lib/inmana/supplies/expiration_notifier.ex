2a6a3dc4bbae2c16d4da3954634fa46887a22d52
Joao Alberto <jooalberto1@gmail.com>
2021-04-24 21:46:27 -0300

    changes notifier send to use async_stream instead Enum

--- lib/inmana/supplies/expiration_notifier.ex
+++ lib/inmana/supplies/expiration_notifier.ex
@@ -5,10 +5,14 @@ defmodule Inmana.Supplies.ExpirationNotifier do
   def send do
     data = GetByExpiration.call()
 
-    Enum.each(data, fn {email, supplies} ->
-      email
-      |> ExpirationEmail.create(supplies)
-      |> Mailer.deliver_later!()
-    end)
+    data
+    |> Task.async_stream(fn {to_email, supplies} -> send_email(to_email, supplies) end)
+    |> Stream.run()
+  end
+
+  defp send_email(to_email, supplies) do
+    to_email
+    |> ExpirationEmail.create(supplies)
+    |> Mailer.deliver_later!()
   end
 end
